347a9e46f55aa0fb8794b5e6f0bbd184337512a7
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-19 15:31:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MongoDBEx-NodsJs
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var mongodb = require("mongodb");
+
+var app = express();
+var mongodbex = mongodb.MongoClient;
+
+//create the RESt API
+app.get("/products",function(req,res){
+    mongodbex.connect("mongodb://localhost:27017/nodedb",
+    function(err,db){
+        db.collection("products").find().toArray(function
+        (err,array){
+            res.send(array);
+        });
+    });
+});
+//Assign the port
+Server.listen(8080);
+console.log("server is listening at port no. 8080");
